b5c0d658d169c10eca1366ee533674e7345d3dcd
Maxym Parfenov <maksim.parfenov@protonmail.ch>
2020-02-18 00:22:06 +0200

    add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,13 +4,23 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._chunkBuffer = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this._chunkBuffer += chunk.toString();
+    callback();
   }
 
   _flush(callback) {
+    const arr = this._chunkBuffer.split(os.EOL);
+    while (arr.length) {
+      this.push(arr.shift());
+    }
+    this._chunkBuffer = '';
+    callback();
   }
 }
+
 
 module.exports = LineSplitStream;
