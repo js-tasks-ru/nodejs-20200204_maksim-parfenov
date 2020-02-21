03285bd3ffc14e73cfc4ddb9f0c23f21eaec0675
mappar <maksim.parfenov@protonmail.ch>
2020-02-21 16:19:56 +0200

    add solution for 4-module 3-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,59 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
+const { createReadStream } = fs;
 const server = new http.Server();
+const { pipeline } = require('stream');
 
+class PathNotExists extends Error {
+  constructor() {
+    super();
+  }
+}
+const set500 = (res) => {
+  res.statusCode = 500;
+  res.end('Server error');
+}
+
+const set404 = (res) => {
+  res.statusCode = 404;
+  res.end('File not found');
+}
+
+
+const systemFiles = ['favicon.ico'];
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
 
+  const pathname = url.parse(req.url).pathname.slice(1);
+  const isDirectlyPath = pathname.split('/').length === 1;
   const filepath = path.join(__dirname, 'files', pathname);
+  const isSystemFiles = systemFiles.every(_ => _ === pathname[0]);
+
+  if (isSystemFiles) {
+    res.statusCode = 200;
+    res.end();
+  }
+
+  if (!isDirectlyPath) {
+    res.statusCode = 400;
+    res.end('Path not exists');
+  }
+
 
   switch (req.method) {
     case 'GET':
+      fs.exists(filepath, (exists) => {
 
+        if (!exists) {
+          return set404(res);
+        }
+
+        const stream = fs.createReadStream(filepath);
+        pipeline(stream, res, (err) => set500(res));
+
+
+      });
       break;
 
     default:
--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,52 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const { existsSync, unlinkSync } = require('fs');
 
 const server = new http.Server();
+const set500 = (res) => {
+  res.statusCode = 500;
+  return res.end('Server error');
+}
+const set413 = (res) => {
+  res.statusCode = 413;
+  return res.end('Server error 413');
+}
 
+const systemFiles = ['favicon.ico'];
+const LIMIT_SIZE = 2 ** 20;
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  const isFilePath = pathname.split('/').length === 1;
+  const isSystemFiles = systemFiles.every(_ => _ === pathname[0]);
+  const isExists = existsSync(filepath);
+
+  if (!isFilePath) {
+    res.statusCode = 400;
+    return res.end('Path not exists');
+  }
+  
+  if (!isExists) {
+    res.statusCode = 404;
+    return res.end('File with same name already exists');
+  }
+
+  if (isSystemFiles) {
+    res.statusCode = 200;
+    return res.end();
+  }
+
+  
+
 
   switch (req.method) {
     case 'DELETE':
-
+      if(existsSync(filepath)){
+        unlinkSync(filepath);
+      }
+      res.statusCode = 200;
+      res.end();
       break;
 
     default:
