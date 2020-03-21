17a8be3870d8ffb9dc07dd98d810583014693a88
mappar <maksim.parfenov@protonmail.ch>
2020-03-21 22:15:54 +0200

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,16 @@
+const Category = require('../models/Category');
+
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+  const collection = categories.map((item) => ({
+    title: item.title,
+    id: item._id,
+    subcategories: item.subcategories.map(({title, _id}) => ({
+      title,
+      id: _id,
+    })),
+  }));
+
+  ctx.body = {categories: collection};
 };
