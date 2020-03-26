cc064c043f30162b18b8b59df94eba5d4011e73d
mappar <maksim.parfenov@protonmail.ch>
2020-03-26 22:04:58 +0200

    add solution for 7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,20 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  // authenticate('github', get(profile, 'emails[0].value'), profile.username, done);
+  // email, displayName - required
+  try {
+    if (!email) return done(null, false, 'Не указан email');
+
+    let user = await User.findOne({email});
+
+    if (user === null) {
+      const newUser = new User({email, displayName});
+      user = await newUser.save();
+    }
+
+    return done(null, user);
+  } catch (err) {
+    done(err);
+  }
 };
