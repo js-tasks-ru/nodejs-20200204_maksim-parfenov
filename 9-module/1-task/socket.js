b99e732e43fd8dbac1c1e564caaee4cee36b40bb
mappar <maksim.parfenov@protonmail.ch>
2020-03-28 10:34:34 +0200

    add solution for 9-module 1-task

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,33 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const {token} = socket.handshake.query;
+
+    if (!token) {
+      return next(new Error('anonymous sessions are not allowed'));
+    }
+
+    const user = Session.findOne({token}).populate('user');
+
+    if (!user) {
+      return next(new Error('wrong or expired session token'));
+    }
+
+    socket.user = user;
+
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async ({from, message}) => {
+      await Message.create({
+        date: new Date(),
+        text: message,
+        chat: socket.user.id,
+        user: socket.user.displayName,
+      });
+      // socket.broadcast.emit('message', {from, message});
+    });
   });
 
   return io;
